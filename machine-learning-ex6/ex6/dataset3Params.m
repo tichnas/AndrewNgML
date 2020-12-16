2efa73e8bcd3d144819176ecec99172b1e7e8df1
tichnas <sanchit.arora.2002@gmail.com>
2020-12-16 23:28:22 +0530

    Ex 6 Sol: Parameters for Dataset 3

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,8 +23,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+vec = [0.01 0.03 0.1 0.3 1 3 10 30];
+minError = size(yval, 1) + 1;
 
+for i = 1:length(vec)
+  for j = 1:length(vec)
+    model = svmTrain(X, y, vec(i), @(x1, x2) gaussianKernel(x1, x2, vec(j)));
+    predictions = svmPredict(model, Xval);
+    error = sum(predictions ~= yval);
 
+    if (error < minError)
+      minError = error;
+      C = vec(i);
+      sigma = vec(j);
+    end
+  end
+end
 
 
 
